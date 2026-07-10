07a178a9ffda31deb8007cfb14d50ad8fe2a258b
Jordan Betts <51442771+jordancb54321@users.noreply.github.com>
2026-07-10 15:58:26 +0100

    Apply Bootswatch Brite theme

--- _worker.js
+++ _worker.js
@@ -14,7 +14,7 @@ export default {
     headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
     headers.set(
       "Content-Security-Policy",
-      "default-src 'self'; script-src 'self' 'wasm-unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; worker-src 'self'; connect-src 'self'; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests",
+      "default-src 'self'; script-src 'self' 'wasm-unsafe-eval'; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; img-src 'self' data:; worker-src 'self'; connect-src 'self'; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests",
     );
     headers.set("Permissions-Policy", "camera=(), microphone=(), geolocation=(), payment=(), usb=(), serial=()");
     headers.set("X-Frame-Options", "DENY");
